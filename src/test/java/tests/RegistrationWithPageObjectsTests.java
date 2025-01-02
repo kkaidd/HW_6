4927842b8e14f81ba706800af516d9f3ac25739a
KMsulimanov <kaborzid@gmail.com>
2025-01-02 22:44:03 +0300

    add it to build.gradle

--- src/test/java/tests/RegistrationWithPageObjectsTests.java
+++ src/test/java/tests/RegistrationWithPageObjectsTests.java
@@ -33,22 +33,5 @@ RegistrationPage registrationPage = new RegistrationPage();
         .verifyResult("Picture", "Screen.png")
         .verifyResult("Address", "Город Москва, улица скворцова-спепанова, дом 15")
         .verifyResult("State and City", "NCR Delhi");
-
-
-
-
-//
-//        $("[class~=table]").shouldHave(text("Student Name")).shouldHave(text("Kaid Sulimanov"));
-//        $("[class~=table]").shouldHave(text("Student Email")).shouldHave(text("kkaidd@mail.ru"));
-//       gradle build --warning-mode all $("[class~=table]").shouldHave(text("Gender")).shouldHave(text("Other"));
-//        $("[class~=table]").shouldHave(text("Mobile")).shouldHave(text("9520931995"));
-//        $("[class~=table]").shouldHave(text("Date of Birth")).shouldHave(text("10 November,1990"));
-//        $("[class~=table]").shouldHave(text("Subjects")).shouldHave(text("English, Hindi"));
-//        $("[class~=table]").shouldHave(text("Hobbies")).shouldHave(text("Sports"));
-//        $("[class~=table]").shouldHave(text("Picture")).shouldHave(text("Screen.png"));
-//        $("[class~=table]").shouldHave(text("Address")).shouldHave(text("Город Москва, улица скворцова-спепанова, дом 15"));
-//        $("[class~=table]").shouldHave(text("State and City")).shouldHave(text("NCR Delhi"));
-//
-
     }
 }
