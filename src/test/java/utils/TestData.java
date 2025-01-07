f5864563ce4e707af2a8ea83a7e40ca4f43681fe
KMsulimanov <kaborzid@gmail.com>
2025-01-08 01:52:03 +0300

    Remove comments

--- src/test/java/utils/TestData.java
+++ src/test/java/utils/TestData.java
@@ -63,25 +63,6 @@ public class TestData {
         return faker.options().option("NCR", "Uttar Pradesh", "Haryana", "Rajasthan");
     }
 
-
-//    public String firstName = faker.name().firstName()
-//    public String lastName = faker.name().lastName();
-//    public String userEmail = faker.internet().emailAddress();
-//    public String gender = faker.options().option("Male", "Female", "Other");
-//    public String telephoneNumber = faker.phoneNumber().subscriberNumber(10);
-////    public String userBirthDay = String.format("%02d", faker.number().numberBetween(1, 28));
-////    public String userBirthMonth = faker.options().option("January", "February", "March", "April",
-////                    "May", "June", "July", "August", "September", "October", "November", "December");
-////    public String userBirthYear = String.format("%s", faker.number().numberBetween(1924, 2024));
-////    public String subject = faker.options().option("Hindi", "English", "Maths", "Physics", "Chemistry", "Biology",
-////                    "Computer Science", "Commerce", "Accounting", "Economics", "Arts", "Social Studies", "History",
-////                    "Civics");
-////    public String hobbies = faker.options().option("Sports", "Reading", "Music");
-////    public String picture = faker.options().option("Screen.png", "Picture Screen.png");
-////    public String userAddress = faker.address().fullAddress();
-////    public String userState = faker.options().option("NCR", "Uttar Pradesh", "Haryana", "Rajasthan");
-//    public String userCity = getRandomCity(userState);
-
     public String getRandomCity(String state) {
         return switch (state) {
             case "NCR" -> faker.options().option("Delhi", "Gurgaon", "Noida");
